6c2def8a16c7b44c43e185f0d0f8c1ca5e0bdb62
ckorir <chrisruto.k@gmail.com>
2023-09-19 13:56:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function mondayWork (activity = 'go to the office'){
     return (`This Monday, I will ${activity}.`);
 }
 
-function wrapAdjective(initialized = "||") {
-    return function(adjective) {
-        return `You are ${initialized}${adjective}${initialized}!`;
+function wrapAdjective(flair = "||") {
+    return function(adjective = 'fast') {
+        return `You are ${flair}${adjective}${flair}!`;
     }
 }
